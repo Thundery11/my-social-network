28569e462ab596bcb58ebe2381ad9a39550fc0bc
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-26 15:40:23 +0300

    ...

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,21 +1,46 @@
 import axios from "axios";
 import React from "react";
-import UserPhoto from '../../assets/images/male-avatar.jpg'
-class Users extends React.Component {
-  // componentDidMount(){
-  //     axios
-  //     .get(`https://social-network.samuraijs.com/api/1.0/users`)
-  //     .then((response) =>{
-  //       this.props.setUsers(response.data.items)
-  //     })
-  // }
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
-}
+import UserPhoto from "../../assets/images/male-avatar.jpg";
+let Users = (props) => {
+  props.setUsers([
+    [
+      {
+        name: "Ebragim55",
+        id: 1096,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+      {
+        name: "uppgraedd",
+        id: 1095,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: "ыва234",
+        followed: false,
+      },
+      {
+        name: "capitan",
+        id: 1094,
+        uniqueUrlName: null,
+        photos: {
+          small: null,
+          large: null,
+        },
+        status: null,
+        followed: false,
+      },
+    ],
+  ]);
+
+
+};
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,41 +1,7 @@
 const SET_USERS = "SET_USERS";
 
 let initialstate = {
-  items: [
-    {
-      name: "Ebragim55",
-      id: 1096,
-      uniqueUrlName: null,
-      photos: {
-        small: null,
-        large: null,
-      },
-      status: null,
-      followed: false,
-    },
-    {
-      name: "uppgraedd",
-      id: 1095,
-      uniqueUrlName: null,
-      photos: {
-        small: null,
-        large: null,
-      },
-      status: "ыва234",
-      followed: false,
-    },
-    {
-      name: "capitan",
-      id: 1094,
-      uniqueUrlName: null,
-      photos: {
-        small: null,
-        large: null,
-      },
-      status: null,
-      followed: false,
-    },
-  ],
+  items: [],
 };
 
 const usersReducer = (state = initialstate, action) => {
