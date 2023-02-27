735f04adcb28a3a8ca7eca93ec50fd61f3f8782a
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-02-27 17:36:10 +0300

    Update Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import React from "react";
 import UserPhoto from "../../assets/images/male-avatar.jpg";
+import s from "./Users.module.css";
 let Users = (props) => {
-  props.setUsers([
-    [
+  if (props.items.length === 0) {
+    props.setUsers([
       {
         name: "Ebragim55",
         id: 1096,
@@ -37,10 +38,55 @@ let Users = (props) => {
         status: null,
         followed: false,
       },
-    ],
-  ]);
+    ]);
+  }
 
-
+  return (
+    <div>
+      {props.items.map((u) => (
+        <div key={u.id}>
+          <span>
+            <div>
+              <img
+                src={u.photos.small != null ? u.photos.small : UserPhoto}
+                alt="sdsds"
+                className={s.userPhoto}
+              />
+            </div>
+            <div>
+              {u.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
+            </span>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
