ff1a9c2c70ed523da129ca2c6e01b989c79fa25a
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-03-02 19:18:25 +0300

    Update Users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,19 +4,18 @@ import UserPhoto from "../../assets/images/male-avatar.jpg";
 import s from "./Users.module.css";
 
 class Users extends React.Component {
-  getUsers = () => {
-    if (this.props.items.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          this.props.setUsers(response.data.items);
-        });
-    }
-  };
+  constructor(props) {
+    super(props);
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+
   render() {
     return (
       <div>
-        <button onClick={this.getUsers}>Get Users</button>
         {this.props.items.map((u) => (
           <div key={u.id}>
             <span>
