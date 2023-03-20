748aa115fcfdf80694acb4cbf21e6b5c55d1f270
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-03-20 16:43:09 +0300

    hoc WithRouter

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,19 +3,38 @@ import React from "react";
 import axios from "axios";
 import { setUserProfile } from "../../redux/profile-reducer";
 import Profile from "./Profile";
+import { useParams } from "react-router-dom";
+export function withRouter(Children) {
+  return (props) => {
+    const match = { params: useParams() };
+    return <Children {...props} match={match} />;
+  };
+}
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    debugger;
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 28039;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         this.props.setUserProfile(response.data);
       });
   }
   render() {
-    return <Profile {...this.props} profile={this.props.profile} />;
+    return (
+      <div>
+        <Profile {...this.props} profile={this.props.profile} />;
+      </div>
+    );
   }
 }
 const mapStateToProps = (state) => {
   return { profile: state.profilePage.profile };
 };
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let withUrlDataContainerComponent = withRouter(ProfileContainer);
+export default connect(mapStateToProps, { setUserProfile })(
+  withUrlDataContainerComponent
+);
