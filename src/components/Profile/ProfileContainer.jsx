a4ee224925436a630cfa2048510b1e723c69f1d0
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-03-27 23:05:29 +0300

    Update ProfileContainer.jsx

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -12,7 +12,6 @@ export function withRouter(Children) {
 }
 class ProfileContainer extends React.Component {
   componentDidMount() {
-    debugger;
     let userId = this.props.match.params.userId;
     if (!userId) {
       userId = 28039;
