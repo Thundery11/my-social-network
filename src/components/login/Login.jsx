06fd646dca55d7f0cbbd89c82bd244ba39adb471
Illia Nizovtsov <66624311+Thundery11@users.noreply.github.com>
2023-04-06 17:20:05 +0300

    feat: added Form for login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,7 +1,50 @@
+import { Field, reduxForm } from "redux-form";
+
+const LoginForm = (props) => {
+  return (
+    <>
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          <Field
+            name="login"
+            component="input"
+            type="text"
+            placeholder="Login"
+          />
+        </div>
+        <div>
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+        <div>
+          <span>
+            <Field name="rememberMe" component="input" type="checkbox" />{" "}
+            Remember me
+          </span>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+const LoginReduxForm = reduxForm({
+  form: "login",
+})(LoginForm);
+
 const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
   );
 };
